9568e6d32b2b27f2d196307bff2c4aaa9ccda75d
jeanmbiz <103115353+jeanmbiz@users.noreply.github.com>
2023-01-04 10:27:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import Global from './styles/global';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
